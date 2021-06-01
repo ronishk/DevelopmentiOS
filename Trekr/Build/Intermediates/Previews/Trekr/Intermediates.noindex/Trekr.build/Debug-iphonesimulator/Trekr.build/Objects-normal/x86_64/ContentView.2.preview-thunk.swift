9ca9f6bd3bf45833ff4e4fcc318782d2f6146397
RonishKhadgi <60719674+RonishKhadgi@users.noreply.github.com>
2021-06-01 16:22:17 -0700

    Adding Json to code

--- Trekr/Build/Intermediates/Previews/Trekr/Intermediates.noindex/Trekr.build/Debug-iphonesimulator/Trekr.build/Objects-normal/x86_64/ContentView.2.preview-thunk.swift
+++ Trekr/Build/Intermediates/Previews/Trekr/Intermediates.noindex/Trekr.build/Debug-iphonesimulator/Trekr.build/Objects-normal/x86_64/ContentView.2.preview-thunk.swift
@@ -4,43 +4,45 @@ import SwiftUI
 
 extension ContentView_Previews {
     @_dynamicReplacement(for: previews) private static var __preview__previews: some View {
-        #sourceLocation(file: "/Users/Ronish/IdeaProjects/Trekr/Trekr/ContentView.swift", line: 45)
-        AnyView(ContentView())
+        #sourceLocation(file: "/Users/Ronish/IdeaProjects/Trekr/Trekr/ContentView.swift", line: 47)
+        AnyView(NavigationView{
+            ContentView(location: Location.example)
+        })
     #sourceLocation()
     }
 }
 
 extension ContentView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
-        #sourceLocation(file: "/Users/Ronish/IdeaProjects/Trekr/Trekr/ContentView.swift", line: 12)
+        #sourceLocation(file: "/Users/Ronish/IdeaProjects/Trekr/Trekr/ContentView.swift", line: 14)
         AnyView(ScrollView{
-            Image(__designTimeString("#810.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: "highlands"))
+            Image(location.heroPicture)
             .resizable()
             .scaledToFit()
         
-            Text(__designTimeString("#810.[1].[0].property.[0].[0].arg[0].value.[1].arg[0].value", fallback: "The Highlands"))
+            Text(location.name)
                 .font(.largeTitle)
                 .bold()
                 .multilineTextAlignment(.center)
             
-            Text(__designTimeString("#810.[1].[0].property.[0].[0].arg[0].value.[2].arg[0].value", fallback: "United Kingdom"))
+            Text(location.country)
                 .font(.title)
                 .foregroundColor(.secondary)
             
-            Text(__designTimeString("#810.[1].[0].property.[0].[0].arg[0].value.[3].arg[0].value", fallback: "The Highlands is a historic region of Scotland. Culturally, the Highlands and the Lowlands diverged from the later Middle Ages into the modern period, when Lowland Scots replaced Scottish Gaelic throughout most of the Lowlands. The term is also used for the area north and west of the Highland Boundary Fault, although the exact boundaries are not clearly defined, particularly to the east. The Great Glen divides the Grampian Mountains to the southeast from the Northwest Highlands. The Scottish Gaelic name of A' Ghàidhealtachd literally means \"the place of the Gaels\" and traditionally, from a Gaelic-speaking point of view, includes both the Western Isles and the Highlands."))
+            Text(location.description)
                 .padding(.horizontal)
             
-            Text(__designTimeString("#810.[1].[0].property.[0].[0].arg[0].value.[4].arg[0].value", fallback: "Did you know?"))
+            Text(__designTimeString("#810.[1].[1].property.[0].[0].arg[0].value.[4].arg[0].value", fallback: "Did you know?"))
                 .font(.title3)
                 .bold()
                 .padding(.top)
             
-            Text(__designTimeString("#810.[1].[0].property.[0].[0].arg[0].value.[5].arg[0].value", fallback: "The area is very sparsely populated, with many mountain ranges dominating the region, and includes the highest mountain in the British Isles, Ben Nevis. During the 18th and early 19th centuries the population of the Highlands rose to around 300,000, but from c. 1841 and for the next 160 years, the natural increase in population was exceeded by emigration (mostly to Canada, the United States, Australia and New Zealand, and migration to the industrial cities of Scotland and England.) The area is now one of the most sparsely populated in Europe. At 9.1/km2 (24/sq mi) in 2012, the population density in the Highlands and Islands is less than one seventh of Scotland's as a whole, comparable with that of Bolivia, Chad and Russia."))
+            Text(location.more)
                 .padding(.horizontal)
             
         }
         
-        .navigationTitle(__designTimeString("#810.[1].[0].property.[0].[0].modifier[0].arg[0].value", fallback: "Discover")))
+        .navigationTitle(__designTimeString("#810.[1].[1].property.[0].[0].modifier[0].arg[0].value", fallback: "Discover")))
     #sourceLocation()
     }
 }
