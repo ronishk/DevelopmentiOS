fbe6b6e87a0d9cb1014eb60c4b013f9f1eefc43c
RonishKhadgi <60719674+RonishKhadgi@users.noreply.github.com>
2021-06-01 19:12:29 -0700

    Map finished

--- Trekr/Trekr/TrekrApp.swift
+++ Trekr/Trekr/TrekrApp.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 @main
 struct TrekrApp: App {
+    @StateObject  var locations = Locations()
+    
     var body: some Scene {
         WindowGroup {
+            TabView{
             NavigationView{
-                ContentView(location: Locations().primary)
+                ContentView(location: locations.primary)
+              }
+            .tabItem {
+                Image(systemName: "airplane.circle.fill")
+                Text("Discover")
+            }
+                NavigationView{
+                    WorldView()
+                }
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Locations")
+                
+                }
+                .environmentObject(locations)
             }
         }
     }
